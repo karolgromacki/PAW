08c0656fe8b5d3f2b9581d56641cd1881b09f6eb
Karol Gromacki <karol.gromacki@gmail.com>
2020-05-16 14:04:19 +0200

    Add footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,16 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     PageNotFoundComponent,
-    FooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
