27c768a730b69066090bfc5de726ab36d4d9a85a
kacperbuczak <kacperbuczak@hotmail.com>
2020-05-23 16:45:42 +0200

    Baskets

--- backend/src/main/java/com/example/backend/repository/BasketRepository.java
+++ backend/src/main/java/com/example/backend/repository/BasketRepository.java
@@ -2,9 +2,16 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.dao.client.Basket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /*
     @author Kacper Buczak 
 */
-public interface BasketRepository extends JpaRepository<Basket,Long> {
+public interface BasketRepository extends JpaRepository<Basket, Long> {
+
+    @Query("Select b from Basket b where b.client.id=:clientId")
+    List<Basket> getBasketsByUser(@Param("clientId") Long clientId);
 }
