53a58ce368581da429dcd7be57aa34ac844463d0
kacperbuczak <kacperbuczak@hotmail.com>
2020-05-23 16:48:42 +0200

    update dto.

--- backend/src/main/java/com/example/backend/entity/dto/client/ClientDto.java
+++ backend/src/main/java/com/example/backend/entity/dto/client/ClientDto.java
@@ -1,13 +1,7 @@
 package com.example.backend.entity.dto.client;
 
-import com.example.backend.entity.dao.client.Basket;
-import com.example.backend.entity.dao.client.Payment;
 import com.example.backend.entity.dto.BaseDto;
 
-import javax.persistence.CascadeType;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 /*
@@ -22,4 +16,71 @@ public class ClientDto extends BaseDto {
     private String NIP;
     private List<BasketDto> baskets;
     private List<PaymentShortDto> payments;
+
+    public ClientDto() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public List<BasketDto> getBaskets() {
+        return baskets;
+    }
+
+    public void setBaskets(List<BasketDto> baskets) {
+        this.baskets = baskets;
+    }
+
+    public List<PaymentShortDto> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<PaymentShortDto> payments) {
+        this.payments = payments;
+    }
 }
