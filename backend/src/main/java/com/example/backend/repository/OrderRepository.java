91a0e46dc3e22d32f19df21f30010a915676787a
kacperbuczak <kacperbuczak@hotmail.com>
2020-05-23 16:12:59 +0200

    yes

--- backend/src/main/java/com/example/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/example/backend/repository/OrderRepository.java
@@ -2,9 +2,17 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.dao.product.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /*
     @author Kacper Buczak 
 */
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+    @Query("select o from Order o where o.basket.id =:basketId")
+    List<Order> getAllOrderByBasketId(@Param("basketId") Long basketId);
 }
