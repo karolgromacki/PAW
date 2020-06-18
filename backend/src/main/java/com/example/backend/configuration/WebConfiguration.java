a3c87a6d7373c83b59392fa3b20b2c1b670d9729
kacperbuczak <kacperbuczak@hotmail.com>
2020-06-18 16:14:54 +0200

    Add web config, jwt filter.

--- backend/src/main/java/com/example/backend/configuration/WebConfiguration.java
+++ backend/src/main/java/com/example/backend/configuration/WebConfiguration.java
@@ -1,18 +1,66 @@
 package com.example.backend.configuration;
 
+import com.example.backend.configuration.filter.JwtRequestFilter;
+import com.example.backend.controller.authentication.JwtAuthenticationEntryPoint;
+import com.example.backend.service.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 /*
     @author Kacper Buczak 
 */
 @Configuration
 @EnableWebMvc
-public class WebConfiguration implements WebMvcConfigurer {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private UserDetailsServiceImpl userDetailsService;
+    private JwtRequestFilter jwtRequestFilter;
+    private DataSource dataSource;
+
+    public WebConfiguration(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                            UserDetailsServiceImpl userDetailsService,
+                            JwtRequestFilter jwtRequestFilter,
+                            DataSource dataSource) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.userDetailsService = userDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select login,password,enabled from clients where login=?")
+                .authoritiesByUsernameQuery("select c.login, r.name from clients c join clients_roles u on c.id = u.clients_id join role r on u.role_id = r.id where c.login = ?");
+
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -31,4 +79,32 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
